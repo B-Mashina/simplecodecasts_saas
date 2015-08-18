b15e723cc33dbf354f3232e2611248ef92775d75
B-Mashina <tony.beaderstadt@gmail.com>
2015-08-18 17:30:57 +0000

    maybe I fized a class

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -1,11 +1,9 @@
-class Contact < ActionMailer::Base
-    default to: 'tony.beaderstadt@gmail.com'
-   
-   def contact_email(name, email, body)
-       @name = name
-       @email = email
-       @body = body
-       
-       mail(from: email, subject: 'Contact Form Message')
-   end
+class ContactMailer < ActionMailer::Base
+  default to: 'your@email_address.com'
+  def contact_email(name, email, body)
+    @name = name
+    @email = email
+    @body = body
+    mail(from: email, subject: 'Contact Form Message')
+  end
 end
